150254c82622081c0e8ac66484488d25e51b9bf5
henriqueaguiiar <henrique.pacheco@dtidigital.com.br>
2025-10-29 12:48:03 -0300

    feat: Product-OrderItem one to many association

--- src/main/java/io/github/henriqueaguiiar/springBoot_project/domain/entities/Product.java
+++ src/main/java/io/github/henriqueaguiiar/springBoot_project/domain/entities/Product.java
@@ -1,5 +1,6 @@
 package io.github.henriqueaguiiar.springBoot_project.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -31,6 +32,10 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
+
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -38,6 +43,15 @@ public class Product implements Serializable {
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem item : items){
+            set.add(item.getOrder());
+        }
+        return set;
     }
 
     @Override
